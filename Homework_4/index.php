ec93b90f6f95549ab8be0035d1e603141c62c658
Aleksandr-Kudriashov <Jertva94@gmail.com>
2021-11-18 22:33:42 +1100

    Dz_4

---
+++ Homework_4/index.php
@@ -0,0 +1,11 @@
+<?
+
+$dir = 'image/';
+$images = scandir($dir);
+for ($i = 0; $i < count($images); $i++) {
+  if ($images[$i] != '.' && $images[$i] != '..') {
+    echo '<a href="' . $dir . $images[$i] . '" target="_blank"><img src=' . $dir . $images[$i] . ' style="width: 200px;"></a>';
+  }
+}
+
+?>
